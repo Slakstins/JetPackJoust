3144f76da7a69626d284a8061c9296a8b3e49b9e
lakstise <lakstise@137.112.238.155>
2019-10-26 19:41:10 -0400

    Retry

--- src/Screen.java
+++ src/Screen.java
@@ -4,7 +4,7 @@ import java.awt.Graphics2D;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
-public abstract class Screen extends JComponent{
+public abstract class Screen{
 	final private int w = 1800; // screen width
 	final private int h = 1000; // screen height
 	private String title;
@@ -14,13 +14,7 @@ public abstract class Screen extends JComponent{
 		this.title = title;
 	}
 	
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		// Get the 2D graphics object
-		Graphics2D g2 = (Graphics2D) g;
-		this.drawEverything(g2);
-		
-	}
+
 	
 	public void main(String[] args) {
 		JFrame frame = new JFrame();
@@ -28,6 +22,7 @@ public abstract class Screen extends JComponent{
 		int frameHeight2 = h;
 
 		frame.setSize(frameWidth2, frameHeight2);
+		frame.add(new DrawComponent());
 		frame.setTitle(this.title);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocation(0, 0);
