3258f8d7cd20dd400487fedf0c47f4a35cb86a0c
lakstise <lakstise@137.112.238.10>
2019-11-10 20:10:10 -0500

    Added Multiplier

---
+++ src/Multiplier.java
@@ -0,0 +1,46 @@
+
+public class Multiplier extends Teleporter {
+	
+	private boolean duplicate;
+
+	public Multiplier(double d, double e) {
+		super(d, e);
+		this.duplicate = false;
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * check to see if duplication should occur
+	 * Occurs at the time of teleportation
+	 * @return
+	 */
+	@Override
+	public boolean checkDuplicate() {
+		if (this.timeToTeleport()) {
+				return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public void updateMovement() {
+		this.checkDuplicate();
+		if (this.timeToTeleport()) {
+			this.teleport();
+//			this.setImage("Multiplier.png");
+		}
+		this.posUpdate();
+		boolean solidify = false;
+		if (this.getIsTeleporting() && this.getX() + 20 > this.getNextX() && this.getX() - 20 < this.getNextX()) {
+			if (this.getY() + 20 > this.getNextY() && this.getY() - 20 < this.getNextY()) {
+				solidify = true;
+			}
+		}
+		if (solidify) {
+			this.solidify();
+		}
+	}
+	
+	
+	
+
+}
